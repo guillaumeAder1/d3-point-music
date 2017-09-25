4646a8d88d333178d0031059f88759cd2d65a47d
Guillaume Ader <GAder@esriuk.com>
2017-09-25 14:42:16 +0100

    add backgroudn color change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 var anim = {
     name: "toto",
+    in:0,
     log: function(){console.log("plpl", this.name, this.container)},
     init: function(params){
         
@@ -9,7 +10,7 @@ var anim = {
         this.container = d3.select('#svg').append('svg')
         .style('width', "100vw")
         .style('height', "100vh");
-        this.container.selectAll('circle').data(this.getData()).enter().append('circle')
+        this.container.selectAll('circle').data(this.getData(36)).enter().append('circle')
             .attr('r', function(d){return d.r})
             .attr('cx', function(d){return d.cx})
             .attr('cy', function(d){return d.cy});
@@ -20,15 +21,27 @@ var anim = {
           
     },
     refreshdata: function(){
-        this.container.selectAll('circle').remove();
-        this.container.selectAll('circle').data(this.getData()).enter().append('circle')
+        this.in += 1;
+        console.log(this.container);
+        this.container.style("background-color", (this.in % 2 === 0) ? 'black' : 'white' )
+        this.container.selectAll('circle').data(this.getData(36)).transition()
+            .duration(250)
             .attr('r', function(d){return d.r})
-            .attr('cx', function(d){return d.cx})
-            .attr('cy', function(d){return d.cy});
+                .style('fill', 'red')
+    //            .style('fill', (this.in % 2 === 0 ) ? 'white' : 'black')
+                .attr('cx', function(d){return d.cx})
+                .attr('cy', function(d){return d.cy});
+        // generate new points
+        // this.container.selectAll('circle').remove();
+        // var newval = this.container.selectAll('circle').data(this.getData(36));
+        // newval.enter().append('circle')
+        //     .attr('r', function(d){return d.r})
+        //     .attr('cx', function(d){return d.cx})
+        //     .attr('cy', function(d){return d.cy});
 
     },
-    getData : function(){
-        var nbrDots = getRandomArbitrary(10,50);
+    getData : function(val){
+        var nbrDots = val || getRandomArbitrary(10,50);
         var arr = new Array(nbrDots).fill({});
         return  arr.map(function(element){
             return{
@@ -36,8 +49,7 @@ var anim = {
                 cx: getRandomArbitrary(0, this.width),
                 cy: getRandomArbitrary(0, this.height)
             }
-        },this)
-       
+        },this);       
     }
 }
 function getRandomArbitrary(min, max) {
