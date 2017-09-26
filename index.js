864bf7063e230132dcd7564b6e66bebf2d5336cf
Guillaume Ader <GAder@esriuk.com>
2017-09-26 09:07:16 +0100

    add color change and tranistion

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 
 var anim = {
     name: "toto",
-    in:0,
+    in:1,
     log: function(){console.log("plpl", this.name, this.container)},
     init: function(params){
         
-        this.width = screen.width;
-        this.height = screen.height;
+        this.width = params.w || screen.width;
+        this.height = params.h || screen.height;
         this.container = d3.select('#svg').append('svg')
         .style('width', "100vw")
         .style('height', "100vh");
-        this.container.selectAll('circle').data(this.getData(36)).enter().append('circle')
+        this._data = this.getData(36)
+        this.container.selectAll('circle').data(this._data).enter().append('circle')
             .attr('r', function(d){return d.r})
             .attr('cx', function(d){return d.cx})
             .attr('cy', function(d){return d.cy});
         
         this.container.on('click', this.refreshdata.bind(this) );
-
-       // d3.select(this.container).selectAll('circle')
-          
     },
     refreshdata: function(){
-        this.in += 1;
-        console.log(this.container);
-        this.container.style("background-color", (this.in % 2 === 0) ? 'black' : 'white' )
-        this.container.selectAll('circle').data(this.getData(36)).transition()
+        this.in += 1;      
+        this.container.transition().duration(250).style("background-color", (this.in % 2 === 0) ? 'black' : 'white' )
+        this.container.selectAll('circle')
+        //.data(this.getData(36))
+        .transition()
             .duration(250)
-            .attr('r', function(d){return d.r})
-                .style('fill', 'red')
-    //            .style('fill', (this.in % 2 === 0 ) ? 'white' : 'black')
-                .attr('cx', function(d){return d.cx})
-                .attr('cy', function(d){return d.cy});
+            .attr('r', function(d){return d.r})                
+                .style('fill', (this.in % 2 === 0 ) ? 'white' : 'black')
+                .attr('cx', function(d){return d.cx + getRandomArbitrary(10,150)})
+                .attr('cy', function(d){return d.cy + getRandomArbitrary(10,150)});
         // generate new points
         // this.container.selectAll('circle').remove();
         // var newval = this.container.selectAll('circle').data(this.getData(36));
